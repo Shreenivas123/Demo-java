0aa1016c376ab3aa099f1a76b1834e9d5c708c9b
Shreenivasa Y G <87532487+Shreenivas123@users.noreply.github.com>
2025-12-07 11:43:12 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,22 +1,16 @@
 package com.example;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.setContentType("text/html; charset=UTF-8");
-        
-        // Use getOutputStream() instead of getWriter()
-        String htmlContent = "<h1>Hello, deployed from CI CD</h1>";
-        resp.getOutputStream().write(htmlContent.getBytes(StandardCharsets.UTF_8));
-        resp.getOutputStream().flush();
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello, deployed from CI CD</h1>");
     }
 }
